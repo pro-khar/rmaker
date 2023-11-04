b46225454e4ebad93faec447b2bb6904e07a7e8d
pro-khar <prakharvermamanu.veed@gmail.com>
2023-11-05 01:21:15 +0530

    init

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Resume maker 
+A static Single column resume maker.
+
+Starting date: **4 November, 2023**
+
+deployment: [CLICK HERE](https://pro-khar.github.io/rmaker)
+
---
+++ script.js
@@ -0,0 +1,14 @@
+
+var experience = {
+    company_name,
+    designation,
+    tenure
+}
+
+experience.company_name = prompt("Where do you work?");
+experience.designation = prompt(`what is your position/designation at ${experience.company_name}?`);
+experience.tenure = prompt("Duration/tenure?");
+
+document.querySelector("#company_name").innerHTML = experience.company_name;
+document.querySelector("#designation").innerHTML = experience.designation;
+document.querySelector("#tenure").innerHTML = experience.tenure;
